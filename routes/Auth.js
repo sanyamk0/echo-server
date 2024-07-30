aa8b66843cc6c754daf4c0593d03ee2a05113391
sanyamk0 <sanyamkumar002@gmail.com>
2024-07-30 23:06:46 +0530

    fixed character casing

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Auth = require("../controllers/auth");
+const Auth = require("../controllers/Auth");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
