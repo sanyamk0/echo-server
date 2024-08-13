9eb959d23d01f52ebb8f06dd4a9d8ed2e85fd84f
sanyamk0 <sanyamkumar002@gmail.com>
2024-08-14 03:16:27 +0530

    fixed image payload issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const corsOption = {
 
 app.use(cors(corsOption));
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "8mb" }));
 
 app.use("/api/auth", authRouter.router);
 app.use("/api/room", roomRouter.router);
